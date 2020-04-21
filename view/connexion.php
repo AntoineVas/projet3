749cdd9fad9e7f467bdb6150c712a1676ef2d6c7
VASONE Antoine <a.vasone@lprs.fr>
2020-04-21 10:38:28 +0200

    Initial commit
    Co-Authored-By: Antoine VASONE <AntoineVas@users.noreply.github.com>

---
+++ view/connexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+  </head>
+  <body>
+    <form class="" action="" method="post">
+      <input type="email" name="mail" value="" placeholder="Mail">
+      <input type="password" name="mdp" value="" placeholder="Mot de Passe">
+      <input type="submit" name="" value="Connexion" placeholder="">
+      <a href="inscription.php">Je n'ai pas de compte - Inscription</a>
+    </form>
+  </body>
+</html>
---
+++ view/inscription.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="" method="post">
+      <input type="text" name="nom" value="" placeholder="Nom">
+      <input type="text" name="prenom" value="" placeholder="Prénom">
+      <input type="email" name="mail" value="" placeholder="Mail">
+      <input type="password" name="mdp" value="" placeholder="Mot de Passe">
+      <input type="submit" name="" value="Inscription" placeholder="">
+      <a href="connexion.php">J'ai un compte - Connexion</a>
+    </form>
+  </body>
+</html>
